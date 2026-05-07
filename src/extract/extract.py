aa5aabb99e76815fdd657b55908a238cc22252b7
JoaoHoffmann <joaohhoffmann16@gmail.com>
2026-05-06 22:53:36 -0300

    feat: adicionando upload dos dados brutos para a s3 Bronze

--- src/extract/extract.py
+++ src/extract/extract.py
@@ -20,3 +20,23 @@ def extrair_dados_crypto():
     return data
 
 
+def salvar_dados_s3(data, bucket_name):
+
+    s3 = boto3.client("s3")
+
+    date = datetime.now().strftime("%Y-%m-%d")
+
+    file_path = f"bronze/{date}/crypto_raw.json"
+
+    s3.put_object(
+        Bucket = bucket_name,
+        Key=file_path,
+        Body=json.dumps(data, indent=2)
+    )
+
+    print(f"Dados salvos na s3://{bucket_name}/{file_path}")
+
+
+if __name__ == "__main__":
+    data = extrair_dados_crypto()
+    salvar_dados_s3(data, "crypto-pipeline-joao")
