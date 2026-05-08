888f99617c7c6204638a7f4aba2c5b07873756c4
JoaoHoffmann <joaohhoffmann16@gmail.com>
2026-05-08 16:48:36 -0300

    refactor: Centraliza nome do bucket em variavel de ambiente

--- src/load/load.py
+++ src/load/load.py
@@ -1,9 +1,16 @@
 import boto3
 import json
 from datetime import datetime
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+BUCKET_NAME = os.getenv("BUCKET_NAME")
 
 
-def salva_na_bronze(data, bucket_name):
+def salva_na_bronze(data):
+    
     s3 = boto3.client("s3")
 
     date = datetime.now().strftime("%Y-%m-%d")
@@ -11,12 +18,12 @@ def salva_na_bronze(data, bucket_name):
     file_path = f"bronze/{date}/crypto_raw.json"
 
     s3.put_object(
-        Bucket = bucket_name,
+        Bucket = BUCKET_NAME,
         Key = file_path,
         Body = json.dumps(data, indent=2)
     )
 
-    print(f"Dados salvos na s3://{bucket_name}/{file_path}")
+    print(f"Dados salvos na s3://{BUCKET_NAME}/{file_path}")
 
 
 
